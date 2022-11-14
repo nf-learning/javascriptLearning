600233a4c9317d9bfa25e9fc40f1864b45cc617a
Nigel Faria <nigel.faria@gmail.com>
2022-11-14 10:24:22 +0000

    tidy up

--- learn/1.js
+++ learn/1.js
@@ -1,12 +1,18 @@
-describe ("pow", function(){
-  it("raises to n-th power", function(){
-    assert.equal(pow(2,3),8);
-    assert.equal(pow(3,4),81);
-    assert.equal(pow(2,-1),0.5);
-    assert.equal(pow(-2,3),-8);
-  });
-});
+// describe ("pow", function(){
+//   it("raises to n-th power", function(){
+//     assert.equal(pow(2,3),8);
+//     assert.equal(pow(3,4),81);
+//     assert.equal(pow(2,-1),0.5);
+//     assert.equal(pow(-2,3),-8);
+//   });
+// });
 
-function pow(x, n) {
-  return x**n;
-}
+// function pow(x, n) {
+//   return x**n;
+// }
+
+console.log(!(""));
+console.log(!(undefined));
+console.log(!null);
+console.log(! "some");
+console.log("" instanceof String);
